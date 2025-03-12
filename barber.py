0717b635d606d6e85c7ae3cc2ad576b375f7307b
SeniorCod-72 <wwekesa72@gmail.com>
2025-03-12 18:45:03 +0300

    Appointment table implemented

--- barber.py
+++ barber.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, create_engine, 
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import DateTime
+from sqlalchemy import datetime 
 
 
 Base = declarative_base()
@@ -21,3 +23,11 @@ class customer(Base):
     phone = Column(String)
 
 
+
+class Appointment(Base):
+    __tablename__ = "appointments"
+
+    id = Column(Integer, primary_key=True)
+    date_time = Column(DateTime, nullable=False, default=datetime.utcnow)
+    barber_id = Column(Integer, ForeignKey('barbers.id'), nullable=False)
+    customer_id = Column(Integer, ForeignKey('customer.id'), nullable=False)
