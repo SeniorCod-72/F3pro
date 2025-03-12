1eff3cf96d5dd8e11b0cf4f36e6e4a41ba70e60f
SeniorCod-72 <wwekesa72@gmail.com>
2025-03-12 18:29:34 +0300

    done some importation and the barber table implemented

---
+++ barber.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, create_engine, 
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class Barber(Base):
+    __tablename__ = "barbers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Integer, primary_key=True)
+
+
+    
